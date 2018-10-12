583d1f6314de928634c789ba24a6b5fb838b8852
Mateusz Domurad <mdomurad@vongg.com>
2018-10-13 00:20:49 +0200

    0.0.4a
    Fixed Footer (now is sticky to bottom)
    Added Notes List
    Added Note View

--- backend/views/notes.php
+++ backend/views/notes.php
@@ -44,6 +44,50 @@
         </div>
       </header>
 
+      <section class="notes-section">
+        <div class="container-fluid">
+          <div class="row">
+
+            <div class="col-12 col-md-4 notes">
+              <h1>Notes</h1>
+
+              <div class="note-item">
+                <div class="row">
+                  <div class="col-9">
+                    <p>2018-10-13</p>
+                    <h1>Example note</h1>
+                  </div>
+                  <div class="col-3 center">
+                    <i class="fas fa-times"></i>
+                  </div>
+                </div>
+              </div>
+
+              <div class="note-item">
+                <div class="row">
+                  <div class="col-9">
+                    <p>2018-10-13</p>
+                    <h1>Example note</h1>
+                  </div>
+                  <div class="col-3 center">
+                    <i class="fas fa-times"></i>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div class="col-12 col-md-8 note">
+              <div class="note-info">
+                <h1>Example note<span>2018-10-13</span></h1>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
       <footer class="main-footer">
         <p>Note Taking &copy; <?= date('Y'); ?></p>
       </footer>
